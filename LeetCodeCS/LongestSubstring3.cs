08713c3bf972575d8849ecf972f53a6bc1bab094
ZclipseCode <bmichaelbusiness@gmail.com>
2023-07-21 11:26:27 -0500

    Add project files.

---
+++ LeetCodeCS/LongestSubstring3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public static class LongestSubstring3
+{
+    public static void Main()
+    {
+        Console.WriteLine(LengthOfLongestSubstring("abcabcbb"));
+        Console.WriteLine(LengthOfLongestSubstring("bbbbb"));
+        Console.WriteLine(LengthOfLongestSubstring("pwwkew"));
+        Console.WriteLine(LengthOfLongestSubstring("dvdf"));
+    }
+
+    static int LengthOfLongestSubstring(string s)
+    {
+        string subStr = "";
+        string longest = "";
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            subStr += s[i];
+
+            if (i < s.Length - 1)
+            {
+                for (int j = 0; j < subStr.Length; j++)
+                {
+                    if (subStr[j] == s[i + 1])
+                    {
+                        if (subStr.Length > longest.Length)
+                        {
+                            longest = subStr;
+                        }
+
+                        string temp = subStr;
+                        subStr = "";
+
+                        for (int k = j + 1; k < temp.Length; k++)
+                        {
+                            subStr += temp[k];
+                        }
+
+                        break;
+                    }
+                }
+            } 
+        }
+
+        if (subStr.Length > longest.Length)
+        {
+            longest = subStr;
+        }
+
+        return longest.Length;
+    }
+}
---
+++ LeetCodeCS/PalindromeNumber9.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public static class PalindromeNumber9
+{
+    public static void Main()
+    {
+        Console.WriteLine(IsPalindrome(101));
+        Console.WriteLine(IsPalindrome(1221));
+        Console.WriteLine(IsPalindrome(4));
+        Console.WriteLine(IsPalindrome(34));
+    }
+
+    static bool IsPalindrome(int x)
+    {
+        string xStr = x.ToString();
+
+        for (int i = 0; i < xStr.Length; i++)
+        {
+            if (i == xStr.Length - i)
+            {
+                continue;
+            }
+            else if (xStr[i] != xStr[xStr.Length - i - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
