6f1fd443b49e0154da6ec7069b32e002e59874e3
ZclipseCode <bmichaelbusiness@gmail.com>
2023-09-14 15:47:08 -0500

    7 reverse integer

---
+++ LeetCodeCS/ReverseInteger7.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ReverseInteger7
+{
+    public static void Main()
+    {
+        Console.WriteLine(Reverse(123));
+        Console.WriteLine(Reverse(-123));
+        Console.WriteLine(Reverse(120));
+    }
+
+    public static int Reverse(int x)
+    {
+        string xStr = x.ToString();
+        bool isNegative = false;
+
+        if (x < 0)
+        {
+            string temp = xStr;
+            xStr = "";
+            isNegative = true;
+
+            for (int i = 1; i < temp.Length; i++)
+            {
+                xStr += temp[i];
+            }
+        }
+
+        List<char> charList = new List<char>();
+
+        for (int i = 0; i < xStr.Length; i++)
+        {
+            charList.Add(xStr[i]);
+        }
+        charList.Reverse();
+
+        string reverseStr = "";
+
+        if (isNegative)
+        {
+            reverseStr = "-";
+        }
+        for (int i = 0; i < charList.Count; i++)
+        {
+            reverseStr += charList[i];
+        }
+
+        if (Int32.TryParse(reverseStr, out int reverse))
+        {
+            return reverse;
+        }
+
+        return 0;
+    }
+}
---
+++ LeetCodeCS/ZigzagConversion6.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ZigzagConversion6
+{
+    public static void Main()
+    {
+        Console.WriteLine(Convert("PAYPALISHIRING", 4));
+    }
+
+    public static string Convert(string s, int numRows)
+    {
+        List<string> rows = Enumerable.Repeat("", numRows).ToList();
+        int currentRow = 0;
+        string zigzag = "";
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            rows[currentRow] += s[i];
+
+            if (currentRow + 1 <= numRows - 1)
+            {
+                currentRow++;
+            }
+            else
+            {
+                currentRow = 0;
+            }
+        }
+
+        for (int i = 0; i < numRows; i++)
+        {
+            for (int j = 0; j < rows[i].Length; j++)
+            {
+                zigzag += rows[i][j];
+            }
+        }
+
+        return zigzag;
+    }
+}
